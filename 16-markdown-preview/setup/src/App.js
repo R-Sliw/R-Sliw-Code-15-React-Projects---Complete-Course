8e4dc5f204592451b062fdedc4dd5e71c7424bde
R-Sliw <robert.sliwinski90@gmail.com>
2022-07-19 21:58:52 +0200

    added lesson 16

--- 16-markdown-preview/setup/src/App.js
+++ 16-markdown-preview/setup/src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-  return <h2>markdown preview starter</h2>
+  const [markdown, setMarkdown] = useState("## markdown preview");
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          type="input"
+          value={markdown}
+          className="input"
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
